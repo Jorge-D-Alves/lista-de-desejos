09b118d1d3d882967936215642098e3bb6bf1bed
Jorge Alves <alvesdjorge@gmail.com>
2022-05-07 15:56:25 -0300

    adicionando itens a lista

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,12 @@
-const Card = () => {
+const Card = ({ item }) => {
 
+  const keywords = item?.title?.split(' ')?.join(',')
   return (
-    <div class="card" style={{ width : '25%'}}>
-      <img src="https://images.lojanike.com.br/1024x1024/produto/tenis-nike-air-max-97-masculino-921826-015-10.png" class="card-img-top" alt="..." />
+    <div class="card" style={{ width : '25%', padding: 0}}>
+      <img src={`https://loremflickr.com/320/240/${keywords}`}
+      />
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">{item?.title}</h5>
         
         <a href="#" class="btn btn-danger">
           Remover da lista
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import Card from '../../components/Card';
 
 const List = () => {
+
+  const [wishItem, setwishItem] = useState('');
+
+  const [wishlist, serWishList] = useState([
+    {
+      id: 1,
+      title: 'Tênis Nike',
+    },
+  ]);
+
+  const addWhishItem = () => {
+    const newwishItem = {
+      id: new Date().getTime(),
+      title: wishItem,
+    };
+    serWishList([...wishlist, newwishItem]);
+  }
+
   return (
     <div className="container">
       <div className="col-6 offset-3 mt-5 text-center">
@@ -10,8 +29,11 @@ const List = () => {
             type="text"
             class="form-control"
             placeholder="Adicione um desejo"
+            onChange={(e) => {
+              setwishItem(e.target.value)
+            }}
           />
-          <button 
+          <button onClick={addWhishItem} 
             class="btn btn-success"
             type="button" 
             >
@@ -20,13 +42,9 @@ const List = () => {
         </div>
       </div>
       <div className='row mt-5'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-
-
+        {wishlist?.map(item =>(
+          <Card item={item}/>
+        ))}
       </div>
     </div>
   );
